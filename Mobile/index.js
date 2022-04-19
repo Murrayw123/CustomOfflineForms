5137753f3cf63abfbe17adf4ca220c602834b82c
Murray Watts <murraycwatts@gmail.com>
2022-04-19 23:28:35 +0800

    add back index.js

---
+++ Mobile/index.js
@@ -0,0 +1,22 @@
+import 'expo-dev-client';
+import 'react-native-get-random-values';
+
+import { registerRootComponent } from 'expo';
+import React from 'react';
+import { Bootstrapper } from 'Bootstrapper';
+import { AppBootstrapper, servicesFactory } from 'services/AppBootstrapper';
+import { MundaBiddiConfiguration } from 'configurations/MundaBiddi';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+
+class App extends React.Component {
+    appBootstrapper = new AppBootstrapper(() => servicesFactory(MundaBiddiConfiguration));
+
+    render() {
+        return <Bootstrapper appBootstrapper={this.appBootstrapper} />;
+    }
+}
+
+registerRootComponent(App);
